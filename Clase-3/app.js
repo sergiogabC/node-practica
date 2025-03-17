6cfb07fd4f84e4edd0a52c78c651a33c44edde7c
sergiogabC <collantesloartesergiogabriel@gmail.com>
2025-03-17 15:21:51 -0500

    segundo commit

--- Clase-3/app.js
+++ Clase-3/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const crypto = require("node:crypto"); //genera id unica
 const movies = require("./movies.json");
+const { validateMovie } = require("./schemas/movies");
 const app = express();
 const PORT = process.env.PORT ?? 1234;
 
 app.disable("x-powered-by"); //deshabilitar el header x-powered-by
 
-app.get("/", (req, res) => {
-  res.json({ message: "hola" });
-});
+app.use(express.json());
 
 app.get("/movies", (req, res) => {
   const { genre } = req.query;
@@ -20,11 +20,31 @@ app.get("/movies", (req, res) => {
   res.json(movies);
 });
 
+//   ab?cd, ab?, a(bc)?d
 app.get("/movies/:id", (req, res) => {
   const { id } = req.params;
   const movie = movies.find((movie) => movie.id === id);
   if (movie) return res.json(movie);
   res.status(404).json({ message: "Movie not found" });
+});
+
+app.post("/movies", (req, res) => {
+  const result = validateMovie(req.body);
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data,
+  };
+
+  //Esto no sería REST, porque estamos guardando el estado de
+  //la aplicación en memoria
+  movies.push(newMovie);
+
+  res.status(201).json(newMovie);
 });
 
 app.listen(PORT, () => {
