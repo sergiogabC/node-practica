0a65869cd969a21ea051f39fd404b702ba5e8f6e
sergiogabC <collantesloartesergiogabriel@gmail.com>
2025-03-20 12:35:35 -0500

    Tercer commit sin comandos

--- Clase-3/app.js
+++ Clase-3/app.js
@@ -1,13 +1,44 @@
 const express = require("express");
 const crypto = require("node:crypto"); //genera id unica
+const cors = require("cors");
 const movies = require("./movies.json");
-const { validateMovie } = require("./schemas/movies");
-const app = express();
+const { validateMovie, validatePartialMovie } = require("./schemas/movies");
 const PORT = process.env.PORT ?? 1234;
+const app = express();
 
 app.disable("x-powered-by"); //deshabilitar el header x-powered-by
-
 app.use(express.json());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        "http://localhost:8080",
+        "http://localhost:1234",
+        "http://192.168.1.222:8080",
+      ];
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true);
+      }
+      if (!origin) {
+        return callback(null, true);
+      }
+      return callback(new Error("Not Allowed by CORS"));
+    },
+  })
+);
+
+app.delete("/movies/:id", (req, res) => {
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: "Movie Not Found" });
+  }
+
+  movies.splice(movieIndex, 1);
+
+  return res.json({ message: "Movie Deleted" });
+});
 
 app.get("/movies", (req, res) => {
   const { genre } = req.query;
@@ -45,6 +76,24 @@ app.post("/movies", (req, res) => {
   movies.push(newMovie);
 
   res.status(201).json(newMovie);
+});
+
+app.patch("/movie/:id", (req, res) => {
+  const result = validatePartialMovie(req.body);
+
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: "Movie not found" });
+  }
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data,
+  };
+
+  movies[movieIndex] = updateMovie;
+  return res.json(updateMovie);
 });
 
 app.listen(PORT, () => {
