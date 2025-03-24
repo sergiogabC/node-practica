177fe66ee706c19faf953b0333f74fe5f3d277e1
sergiogabC <collantesloartesergiogabriel@gmail.com>
2025-03-24 15:06:26 -0500

    Cambios en la practica terminar el cors

--- Practica/4.cors.js
+++ Practica/4.cors.js
@@ -3,6 +3,10 @@ const app = express();
 const redSocialData = require("./Json/redSocial.json");
 const PORT = process.env.PORT ?? 1234;
 const cors = require("cors");
+const depurarPerfiles = require("./4.cors/depurarPerfiles");
+const perfiles = depurarPerfiles(redSocialData);
+
+app.disable("x-powered-by");
 
 app.use(cors());
 
@@ -11,20 +15,30 @@ app.get("/", (req, res) => {
 });
 
 app.get("/perfiles", (req, res) => {
-  let firstPerfiles = Object.values(redSocialData);
-  let perfiles = [];
-  firstPerfiles.forEach((perfil) => {
-    perfiles.push({
-      id: perfil._id,
-      nombre: perfil.name,
-      edad: perfil.age,
-      correo: perfil.email,
-      telefono: perfil.phone,
-      amigos: perfil.friends,
-    });
-    console.log(Object.values(perfiles));
-  });
   res.json(perfiles);
+});
+
+app.delete("/perfiles/:id", (req, res) => {
+  const { id } = req.params;
+  const perfilIndex = perfiles.findIndex((perfil) => perfil.id === id);
+  console.log(perfilIndex);
+  if (perfilIndex === -1) {
+    return res.status(404).json("MOVIE NOT FOUND");
+  }
+  perfiles.splice(perfilIndex, 1);
+  res.status(200).json("Movie Deleted");
+});
+
+//Eliminar Amigo
+app.delete("/perfiles/:id/:idAmigo", (req, res) => {
+  const { id } = req.params;
+  const { idAmigo } = req.params;
+  const perfilIndex = perfiles.findIndex((perfil) => perfil.id === id);
+  const amigos = perfiles[perfilIndex].amigos[idAmigo];
+
+  console.log(amigos);
+
+  res.status(200).json("Movie Deleted");
 });
 
 app.listen(PORT, () => {
